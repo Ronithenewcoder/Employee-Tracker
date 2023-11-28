e93a783768484a5df925a0fd06ee291211eaee86
Selecia aaa <garrisonroniece@gmail.com>
2023-11-28 17:19:06 -0500

    comments added code started

---
+++ server.js
@@ -0,0 +1,68 @@
+const inquirer = require("inquirer");
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'admin',
+    database: 'employees_db',
+},
+    console.log('Conected to database!'));
+
+
+
+db.connect(function (err) {
+    if (err) throw err;
+    startMenu();
+});
+
+const startMenu = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "menu",
+            message: "What would you like to do?",
+            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles','Quit'],
+        }
+    ]).then((res) => {
+
+        switch (res.menu) {
+            case 'View All Employees':
+                viewEmployees()
+                break;
+            case 'Add Employee':
+                addEmployee()
+
+                break;
+            case 'Update Employee Role':
+                updateEmployeeRole()
+                break;
+            case 'View All Roles':
+                viewRoles()
+                break;
+            case 'Quit':
+                db.end();
+                break;
+        }
+    })
+}
+
+// view all employees
+const viewEmployees = () => {
+   
+};
+
+// add a new employee
+const addEmployee = () => {
+   
+};
+
+// update an employees role
+const updateEmployeeRole = () => {
+    
+};
+
+// Function to view all roles
+const viewRoles = () => {
+  
+};
