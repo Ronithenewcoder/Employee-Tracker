eea7f94e35889ef07c89212d57a787e2c4fa498d
Selecia aaa <garrisonroniece@gmail.com>
2023-11-28 18:40:27 -0500

    everythings working, test passed and completed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,12 @@
---roles
-INSERT INTO roles (title, salary, department_id) VALUES
-  ('HR Manager', 60000.00, 1),
-  ('Software Engineer', 80000.00, 2),
-  ('Sales Representative', 50000.00, 3);
+/* roles */
+INSERT INTO roles (title, salary) VALUES
+  ('HR Manager', 60000.00),
+  ('Software Engineer', 800000.00),
+  ('Sales Representative', 70000.00);
 
---employees
+/* employees */
 INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES
-  ('John', 'Doe', 2, NULL),
+   ('John', 'Doe', 2, NULL),
   ('Jane', 'Smith', 3, 1),
-  ('Bob', 'Johnson', 2, 1),
-  ('Alice', 'Williams', 3, 1);
+  ('Sher', 'Johnson', 2, 1),
+  ('Allyssa', 'Cohan', 3, 1);
